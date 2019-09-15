2af648a7b6e48db2288411a3b20c5231b1343bb3
Hector A. Acosta <hector.acosta.pozo@gmail.com>
2019-09-15 17:37:18 -0400

    Removed files related to rest API

--- .travis/kubernetes.sh
+++ .travis/kubernetes.sh
@@ -1,5 +1,5 @@
 # kubernetes.sh
 #!/bin/sh
 
-mkdir ${HOME}/.kube
-doctl kubernetes cluster kubeconfig save $CLUSTER_NAME
+# mkdir ${HOME}/.kube
+# doctl kubernetes cluster kubeconfig save $CLUSTER_NAME
