60892ec452eb7aae6559565f7d0aabe14a6d7790
Carlos Eduardo Valdez Quintin <carlos@carlosvq.com>
2019-10-01 22:59:28 -0400

    Add: Husky and Prettier

--- api/graphql/resolvers/bucket.resolvers.ts
+++ api/graphql/resolvers/bucket.resolvers.ts
@@ -1,23 +1,26 @@
 module.exports = {
   Bucket: {
     parent(parent) {
-      return prisma.bucket({ id: parent.id }).parent()
+      return prisma.bucket({ id: parent.id }).parent();
     },
     owner(parent) {
-      return prisma.bucket({ id: parent.id }).owner()
+      return prisma.bucket({ id: parent.id }).owner();
     },
     childs(parent) {
-      return prisma.buckets({ where: { parent: { id: parent.id } } })
+      return prisma.buckets({ where: { parent: { id: parent.id } } });
     },
     async level(parent) {
-      let deepness = 0
-      const childsArray = await prisma.buckets({ where: { parent: { id: parent.id } } })
+      let deepness = 0;
+      const childsArray = await prisma.buckets({
+        where: { parent: { id: parent.id } }
+      });
       for (var i = 0; i < childsArray.length; i++) {
-        console.log(childsArray)
-        deepness = 1 + (await prisma.bucket({ id: childsArray[i].id })['level'] || 0)
+        console.log(childsArray);
+        deepness =
+          1 + ((await prisma.bucket({ id: childsArray[i].id })["level"]) || 0);
       }
 
-      return deepness
+      return deepness;
     }
   }
-}
+};
