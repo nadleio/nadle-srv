b3e7659cfd6a9ba357e8edb1d3b59d50ecceba3b
Hector A. Acosta <hector.acosta.pozo@gmail.com>
2020-02-09 21:50:43 -0400

    Initiated connection to ElasticSearch server

--- server.ts
+++ server.ts
@@ -13,6 +13,29 @@ const graphql: GraphQLServer = new GraphQLServer({
   middlewares: [permissions]
 });
 
+var bonsai_url = process.env.BONSAI_URL;
+var elasticsearch = require("elasticsearch");
+var client = new elasticsearch.Client({
+  host: bonsai_url,
+  log: "trace"
+});
+
+// Test the connection:
+// Send a HEAD request to "/" and allow
+// up to 30 seconds for it to complete.
+client.ping(
+  {
+    requestTimeout: 30000
+  },
+  function(error) {
+    if (error) {
+      console.error("elasticsearch cluster is down!");
+    } else {
+      console.log("All is well");
+    }
+  }
+);
+
 const options = {
   port: portGraphql,
   endpoint: "/graphql",
